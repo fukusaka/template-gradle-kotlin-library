860d211a6883470f2cf5ab60620d25c50f968bf9
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2021-12-05 19:19:56 +0900

    sample code

---
+++ src/main/java/Hello.kt
@@ -0,0 +1,16 @@
+@Suppress("unused")
+object Hello {
+    /**
+     * message
+     */
+    @JvmStatic
+    val message = "Hello World!"
+
+    /**
+     * fetch message
+     *
+     * @return message
+     */
+    @JvmStatic
+    fun fetchMessage(): String = message
+}
