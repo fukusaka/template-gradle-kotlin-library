65e91fc9dca0ffcd6dfff703e4fa8acdbaeb8d4b
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2021-12-05 19:21:27 +0900

    add github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.0"
+    id("maven-publish")
 }
 
 group = "org.example"
@@ -11,4 +12,29 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("Jar") {
+            from(components["java"])
+            artifactId = "hello"
+        }
+    }
+
+    repositories {
+        val uploadMavenUser = project.findPropertyAsString("uploadMavenUser")
+        val uploadMavenPassword = project.findPropertyAsString("uploadMavenPassword")
+
+        if (uploadMavenUser != null && uploadMavenPassword != null) {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/fukusaka/template-gradle-kotlin-library")
+                credentials {
+                    username = uploadMavenUser
+                    password = uploadMavenPassword
+                }
+            }
+        }
+    }
 }
