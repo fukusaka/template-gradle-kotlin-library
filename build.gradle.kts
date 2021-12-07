1f915f085de39bef109eb31952de1ff4c0a6c34d
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2021-12-07 23:30:12 +0900

    add JUnit5

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
 }
 
 publishing {
