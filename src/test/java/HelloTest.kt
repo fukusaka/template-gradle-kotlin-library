4749b0231a70bbefcdfe11c7c673db6a027a2aea
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2021-12-07 23:47:45 +0900

    add unit test

---
+++ src/test/java/HelloTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class HelloTest {
+
+    @Test
+    fun getMessage() {
+        assertEquals(Hello.message, "Hello World!")
+    }
+
+    @Test
+    fun fetchMessage() {
+        assertEquals(Hello.fetchMessage(), "Hello World!")
+    }
+}
