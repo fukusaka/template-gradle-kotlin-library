254f48cf63ba8d6886c63021c9d26a04f9388e87
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2021-12-11 01:19:52 +0900

    org.example パッケージに移動

--- src/main/java/org/example/Hello.kt
+++ src/main/java/org/example/Hello.kt
@@ -1,3 +1,5 @@
+package org.example
+
 @Suppress("unused")
 object Hello {
     /**
--- src/test/java/org/example/HelloTest.kt
+++ src/test/java/org/example/HelloTest.kt
@@ -1,3 +1,5 @@
+package org.example
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
